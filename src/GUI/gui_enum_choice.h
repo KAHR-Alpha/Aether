475053cb582ac4772b3a03eec0a6d1a8c8fb17c9
KAHR-Alpha <113597714+KAHR-Alpha@users.noreply.github.com>
2025-07-16 20:04:26 +0200

    Improvements to the tabulated material data panel

--- src/GUI/gui_enum_choice.h
+++ src/GUI/gui_enum_choice.h
@@ -15,8 +15,12 @@ limitations under the License.*/
 #ifndef GUI_ENUM_CHOICE_H
 #define GUI_ENUM_CHOICE_H
 
-#include <wx/choice.h>
+#include <aether.h>
 
+#include <wx/choice.h>
+#include <wx/dialog.h>
+
+#include <optional>
 #include <vector>
 
 template<typename T>
@@ -52,6 +56,76 @@ class EnumChoice: public wxChoice
                     return;
                 }
             }
+        }
+};
+
+
+template<typename T>
+class EnumRadioDialog: public wxDialog
+{
+    public:
+        std::vector<T> items;
+        std::optional<T> selection;
+        
+        wxRadioBox *selector;
+        
+        EnumRadioDialog(wxString const &title,
+                        std::vector<wxString> const &labels,
+                        std::vector<T> const &items_)
+            :wxDialog(0,wxID_ANY,"",
+                      wxGetApp().default_dialog_origin()),
+             items(items_)
+        {
+            wxBoxSizer *sizer = new wxBoxSizer(wxVERTICAL);
+            
+            // Selector
+            
+            const int N=labels.size();
+    
+            wxArrayString choices;
+            choices.Alloc(N);
+            
+            for(int i=0;i<N;i++)
+            choices.Add(labels[i]);
+            
+            selector = new wxRadioBox(this, wxID_ANY, title, wxDefaultPosition, wxDefaultSize, choices, 0, wxVERTICAL);
+            
+            sizer->Add(selector);
+            
+            // Buttons
+            
+            wxBoxSizer *btn_sizer = new wxBoxSizer(wxHORIZONTAL);
+
+            wxButton *ok_btn = new wxButton(this, wxID_ANY, "Ok");
+            ok_btn->Bind(wxEVT_BUTTON, &EnumRadioDialog<T>::evt_ok, this);
+            
+            wxButton *cancel_btn = new wxButton(this, wxID_ANY, "Cancel");
+            cancel_btn->Bind(wxEVT_BUTTON, &EnumRadioDialog::evt_cancel, this);
+            
+            btn_sizer->Add(ok_btn);
+            btn_sizer->Add(cancel_btn);
+            
+            sizer->Add(btn_sizer, wxSizerFlags().Align(wxALIGN_RIGHT));
+            
+            SetSizerAndFit(sizer);
+        }
+            
+        void evt_cancel(wxCommandEvent &event)
+        {
+            Close();
+        }
+        
+        void evt_ok(wxCommandEvent &event)
+        {
+            int ID=selector->GetSelection();
+            selection = items[ID];
+            
+            Close();
+        }
+        
+        std::optional<T> get_selection() const
+        {
+            return selection;
         }
 };
 
